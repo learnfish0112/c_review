5e7712a21bb26df4f77dc497650908e560712cf2
tml <18655532848@163.com>
2025-05-24 20:08:19 +0800

    2025/05/24 20:07 input 2 fraction at one time

---
+++ c-base-day02/coding_qid6_fraction_add.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, denom1, num2, denom2;
+    scanf("%d / %d + %d / %d", &num1, &denom1, &num2, &denom2);
+    printf("%d %d %d %d", num1, denom1, num2, denom2);
+    return 0;
+}
+

b243ea99c9d6e7a99848772227892f89f6ad1d52
tml <18655532848@163.com>
2025-07-03 09:37:30 +0800

    revisal pointer exercise

--- c-boost-day01/pointer_exercise/1_u11_6.c
+++ c-boost-day01/pointer_exercise/1_u11_6.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #define ARR_SIZE(a) (sizeof(a)/sizeof(a[0]))
 
@@ -7,6 +8,7 @@ void find_two_largest(int a[], int n, int *plargest, int *psecond_largest);
 int main()
 {
     int arr[] = {1, 3, 5, 8, 2, 4, 6};
+    /* int arr[] = {1}; */
     int largest = -1, second_largest = -1;
 
     find_two_largest(arr, ARR_SIZE(arr), &largest, &second_largest);
@@ -15,14 +17,18 @@ int main()
 }
 
 void find_two_largest(int a[], int n, int *plargest, int *psecond_largest) {
+    bool flag = false;
+
     for(int i = 0; i < n; i++) {
         if(a[i] > *plargest) {
-            if(*plargest = *psecond_largest) {
+            if(*plargest == *psecond_largest && flag == false) {
                 *psecond_largest = a[i];
+                flag = true;
             } else {
                 *psecond_largest = *plargest;
-                *plargest = a[i];
+                /* printf("%d\n", *plargest); */
             }
+            *plargest = a[i];
         } else if(a[i] > *psecond_largest) {
             *psecond_largest = a[i];
         }

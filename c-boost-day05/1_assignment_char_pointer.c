ba89aa9ade8621613e21433bd2594458018fad17
tml <18655532848@163.com>
2025-07-09 14:19:37 +0800

    C string assignment && IO

---
+++ c-boost-day05/1_assignment_char_pointer.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    /* char str[9] = "June 14"; */
+    char* cp = "abc";
+    /* cp = "abc"; */
+
+    printf("%s\n", cp);
+    return 0;
+}
+
---
+++ c-boost-day05/2_scanf_str.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[10];
+
+    scanf("%s", str);
+    printf("%s\n", str);
+    printf("%c",getchar());
+    return 0;
+}
+

27e238494ef542e76918ba5bd72575b26fe6a713
tml <18655532848@163.com>
2025-07-13 15:32:26 +0800

    strarr

---
+++ c-boost-day06/0_strarr.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    char *strarr[] = {"Earth",
+                    "Pluto"};
+
+    /* strarr[0][0] = 'D'; Wrong*/
+    printf("hello\n");
+
+    printf("strarr = %s\n", strarr[0]);
+    return 0;
+}
+
---
+++ c-boost-day06/1_literal_string_pointer.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    char* p = "hello";
+    p = "abc";
+
+    printf("%s\n",p);
+    return 0;
+}
+

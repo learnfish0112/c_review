14bf33775b516d65f60188e690a3109fe6bfb023
tml <18655532848@163.com>
2025-09-21 16:34:54 +0800

    tmpfile

---
+++ c-lib-day04/0_tempfile.c
@@ -0,0 +1,27 @@
+// crt_tmpfile.c
+// compile with: /W3
+// This program uses tmpfile to create a
+// temporary file, then deletes this file with _rmtmp.
+#include <stdio.h>
+
+int main( void )
+{
+   FILE *stream;
+   int  i, ret;
+
+   // Create temporary files.
+   for( i = 1; i <= 3; i++ )
+   {
+      if( (stream = tmpfile()) == NULL ) // C4996
+      // Note: tmpfile is deprecated; consider using tmpfile_s instead
+         perror( "Could not open new temporary file\n" );
+      else
+         printf( "Temporary file %d was created\n", i );
+         ret = fclose(stream);
+         // Remove temporary file if we close it.
+         printf("fclose stream %d ret %d\n", i, ret);
+   }
+
+   // Remove temporary files after program exit.
+   return 0;
+}
--- pushlab.sh
+++ pushlab.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
  git add .
- git commit -m "fopen"
+ git commit -m "tmpfile"
  git push
  git branch --all 
